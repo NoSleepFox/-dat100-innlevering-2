cfcca37c8970bf4c7559cb1081fe4666699246d8
NoSleepFox <SimenCodemaster@proton.me>
2025-09-29 17:35:57 +0200

    finnished a) and b) in Matriser.java

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -2,51 +2,64 @@ package no.hvl.dat100.matriser;
 
 public class Matriser {
 
-	// a)
-	public static void skrivUt(int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skrivUt ikke implementert");
-	}
+    // a)
+    public static void skrivUt(int[][] matrise) {
 
-	// b)
-	public static String tilStreng(int[][] matrise) {
+        for (int i = 0; i < matrise.length; i++) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-		
-	}
+            String matrise_linje = "";
 
-	// c)
-	public static int[][] skaler(int tall, int[][] matrise) {
-		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
-	}
+            for (int j = 0; j < matrise[i].length; j++) {
+                matrise_linje += matrise[i][j] + " ";
+            }
 
-	// d)
-	public static boolean erLik(int[][] a, int[][] b) {
+            System.out.println(matrise_linje);
+        }
+    }
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden erLik ikke implementert");
-		
-	}
-	
-	// e)
-	public static int[][] speile(int[][] matrise) {
+    // b)
+    public static String tilStreng(int[][] matrise) {
 
-		// TODO
+        String matrise_string = "";
+        for (int i = 0; i < matrise.length; i++) {
+            for (int j = 0; j < matrise[i].length; j++) {
+                matrise_string += matrise[i][j] + " ";
+            }
+            matrise_string += "\n";
+        }
 
-		throw new UnsupportedOperationException("Metoden speile ikke implementert");
-	
-	}
+        return matrise_string;
+    }
 
-	// f)
-	public static int[][] multipliser(int[][] a, int[][] b) {
+    // c)
+    public static int[][] skaler(int tall, int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
-	
-	}
+        // TODO
+        throw new UnsupportedOperationException("Metoden skaler ikke implementert");
+
+    }
+
+    // d)
+    public static boolean erLik(int[][] a, int[][] b) {
+
+        // TODO
+        throw new UnsupportedOperationException("Metoden erLik ikke implementert");
+
+    }
+
+    // e)
+    public static int[][] speile(int[][] matrise) {
+
+        // TODO
+        throw new UnsupportedOperationException("Metoden speile ikke implementert");
+
+    }
+
+    // f)
+    public static int[][] multipliser(int[][] a, int[][] b) {
+
+        // TODO
+        throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
+
+    }
 }
